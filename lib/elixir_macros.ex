1e40b5dbc962c99e8d796b01aa274016baec0827
Matej Minarik <mato.minarik@azet.sk>
2018-06-10 16:54:14 +0200

    Add Unless

---
+++ lib/elixir_macros.ex
@@ -0,0 +1,18 @@
+defmodule ElixirMacros do
+  @moduledoc """
+  Documentation for ElixirMacros.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirMacros.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
